e29eee6a55cc6b5ae84b278411da8a3ecec4884b
maciek <maciek@maciek>
2013-07-12 13:01:43 +0200

    much

--- ssh.py
+++ ssh.py
@@ -1,8 +1,28 @@
 import os
 import libssh2
+import socket
 
 class sshClass:
-  def connect(self,un,pw,host):
-    cmd='konsole --hold --noclose -e "sshpass -p '+pw+' ssh '+un+'@'+host+'" '
-    print cmd
+  def command(self,cmd):
+    self.channel.execute(cmd)
+    return self.channel.read(1024) #beware of the limit
+  
+  def setData(self,data):
+    self.data=data
+  
+  def connect(self):
+    un,pw,host=self.data
+    print "Connection to"
+    print un+"@"+host
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.connect((host, 22))
+    session = libssh2.Session()
+    session.startup(self.sock)
+    session.userauth_password(un, pw)
+    self.channel = session.open_session()
+   
+  def disconnect(self):
+     print "Disconnecting"
+     self.sock.close()
+    
     
--- sshwindow.py
+++ sshwindow.py
@@ -1,14 +1,30 @@
+import gtk
+
+
 class sshWindow:
+  
+  def keypress(self, widget, event):
+    print "test"
+    if event.keyval == gtk.keysyms.Return:
+      print "ok"
+  
   def delete_event(self, widget, event, data=None):
-    return False
+    self.ssh.disconnect()
+   
   def destroy(self, widget, data=None):
     #self.window.hide() #WARNING
+    
     return False
-  def __init__(self,builder):
+  def __init__(self,builder,ssh):
     self.builder=builder
     self.window=builder.get_object('sshWindow')
+    self.cmdLine=builder.get_object('cmdLine')
+    self.cmdLine.connect("key-press-event",self.keypress)
+    self.ssh=ssh
     builder.connect_signals(self)
+ 
   def __del__(self):
     pass
   def show(self):
+    self.ssh.connect()
     self.window.show()
